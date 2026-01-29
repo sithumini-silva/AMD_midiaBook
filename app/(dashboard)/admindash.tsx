a6f7bd5fd0f2b623191b8309a2ffcaeccc0741dc
sithumini-silva <sithusilva94@gmail.com>
2026-01-29 12:52:42 +0530

    done admin dash

---
+++ app/(dashboard)/admindash.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import ProtectedRoute from "../rolebase/ProtectedRoute";
+
+export default function AdminDash() {
+  return (
+    <ProtectedRoute role="admin">
+      <View>
+        <Text style={{ fontSize: 24 }}>Admin Dashboard</Text>
+        <Text>• Add Doctors</Text>
+        <Text>• View Users</Text>
+        <Text>• View Appointments</Text>
+      </View>
+    </ProtectedRoute>
+  );
+}
