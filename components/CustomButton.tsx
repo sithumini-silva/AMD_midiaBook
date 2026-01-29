73966900714faac238bb5a7d3c940f454f1da1b1
sithumini-silva <sithusilva94@gmail.com>
2026-01-29 12:54:21 +0530

    done custom Buttons

---
+++ components/CustomButton.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export default function CustomButton({ title, onPress }: any) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        backgroundColor: "#f97316",
+        padding: 12,
+        borderRadius: 8,
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ color: "#fff", fontWeight: "600" }}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
