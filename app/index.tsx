4565a0eacdf9b593e8c11a3bc570b04d7197ea2a
sithumini-silva <sithusilva94@gmail.com>
2026-01-29 12:51:45 +0530

    done _layout

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useRouter } from "expo-router";
+import { useAuth } from "../hooks/useAuth";
+import Loader from "../components/Loader";
+
+export default function Index() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading) {
+      if (!user) router.replace("/login");
+      else if (user.role === "admin") router.replace("/admindash");
+      else if (user.role === "doctor") router.replace("/doctordash");
+      else router.replace("/patientdash");
+    }
+  }, [user, loading]);
+
+  if (loading) return <Loader />;
+
+  return null;
+}
