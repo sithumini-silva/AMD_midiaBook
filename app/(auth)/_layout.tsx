b9ab09ac63313994a0858d0208cb5e51932f6e88
sithumini-silva <sithusilva94@gmail.com>
2026-01-29 13:05:20 +0530

    update _layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,5 @@
-import React from "react";
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false, // hide header for auth screens
-        animation: "slide_from_right", // smooth transition
-      }}
-    />
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
